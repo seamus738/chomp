2c3284bb2eeac260c2c3eefd7a38846de5fddb67
seamus738 <m265916@usna.edu>
2023-10-12 09:45:26 -0400

    first game update

--- game.py
+++ game.py
@@ -1,2 +1,26 @@
-print('hello world')
-print('new message!')
+import pygame
+import time
+
+print(f'the quit event is type {pygame.QUIT}')
+#Quit=256
+pygame.init()
+
+screen=pygame.display.set_mode((400,400))
+pygame.display.set_caption("Chomp!!")
+
+# for color
+screen.fill((100,200,255))
+
+#make a rectangle
+pygame.draw.rect(screen, (100,25,0), (0,380, 400, 400))
+pygame.draw.rect(screen,(100,200,0), (200, 200, 25, 25))
+pygame.display.flip()
+
+while True:
+    #could use: if event.type==pygame.QUIT: or -->
+    for event in pygame.event.get():
+        if event.type==256:
+            print('not today')
+        print(event.type)
+
+
