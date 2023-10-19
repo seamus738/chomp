aee8d8ef8fefd4adc0daa40f8a36412888446672
seamus738 <m265916@usna.edu>
2023-10-19 08:52:42 -0400

    added fonts

--- game.py
+++ game.py
@@ -1,20 +1,15 @@
 import pygame
 import sys
+import random
 
 print(f'the quit event is type {pygame.QUIT}')
 # Quit=256
 
-# DIMENSIONS
-TITLE_SIZE = 64
-SCREEN_WIDTH = 8*TITLE_SIZE
-SCREEN_HEIGHT = 8*TITLE_SIZE
-SAND_HEIGHT = 20
+from settings import *
 
 pygame.init()
 
-# COLORS
-WATER_COLOR = (100, 200, 255)
-SAND_COLOR = (100, 25, 0)
+game_font=pygame.font.Font('assets/fonts/Black_Crayon.ttf', 128)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chomp!!")
@@ -25,7 +20,7 @@ screen.fill(WATER_COLOR)
 # make a rectangle
 pygame.draw.rect(screen, SAND_COLOR, (0, SCREEN_HEIGHT - SAND_HEIGHT,
                                       SCREEN_WIDTH, SAND_HEIGHT))
-pygame.draw.rect(screen, (100, 200, 0), (200, 200, 25, 25))
+#pygame.draw.rect(screen, (100, 200, 0), (200, 200, 25, 25))
 sand = pygame.image.load("assets/images/sand.png").convert()
 # .convert() will make an image transparent
 
@@ -33,12 +28,22 @@ for i in range(SCREEN_WIDTH // TITLE_SIZE):
     screen.blit(sand, (TITLE_SIZE*i, SCREEN_HEIGHT-TITLE_SIZE))
 # BLIT SAND TITLES ACROSS THE BOTTOM OF THE SCREEN
 sandtop = pygame.image.load("assets/images/sand_top.png").convert()
+# .convert() will make an image transparent
 sandtop.set_colorkey((0, 0, 0))
 # use this to fill the black pixel with the background
 for i in range(SCREEN_WIDTH // TITLE_SIZE):
     screen.blit(sandtop, (TITLE_SIZE*i, SCREEN_HEIGHT-2*TITLE_SIZE))
 
-# .convert() will make an image transparent
+seaweed = pygame.image.load("assets/images/seagrass.png").convert()
+seaweed.set_colorkey((0, 0, 0))
+for _ in range(4):
+    y=random.randint(SCREEN_HEIGHT-2*TITLE_SIZE, SCREEN_HEIGHT)-(.5*TITLE_SIZE)
+    x=random.randint(0, SCREEN_WIDTH)
+    screen.blit(seaweed, (x, y))
+#draw chomp title
+text=game_font.render("Chomp!", True, (255, 69, 0))
+
+screen.blit(text, (SCREEN_WIDTH//2-text.get_width()//2,SCREEN_HEIGHT//2-text.get_height()//2))
 pygame.display.flip()
 
 while True:
